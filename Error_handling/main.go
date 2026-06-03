ba7fd494f3ab657e4b38324bb1a36ed327bd28f5
Gagandeep <editoranonymous47@gmail.com>
2026-06-03 18:31:31 +0530

    Error Handling basic

--- Error_handling/main.go
+++ Error_handling/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func divide(a, b float64) (float64, string) {
+	if b == 0 {
+		err := "denominator should not be zero"
+		return 0, ("Error: " + err)
+	}
+	return a / b, "nil"
+}
+func main() {
+	fmt.Println("Starting Error handling")
+
+	div, err := divide(10, 0)
+
+	fmt.Println("Division is ", div)
+
+	fmt.Println(err)
 }
