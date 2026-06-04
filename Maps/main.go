25860d3f50d4866c93b4e782d127c92b620cd6f1
Gagandeep <editoranonymous47@gmail.com>
2026-06-04 14:55:25 +0530

    Maps Basics

---
+++ Maps/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Starting Maps in Go")
+
+	// Creating a map
+	balance := make(map[string]int)
+
+	// Adding key-value pairs to the map
+	balance["Gagan"] = 10000000
+	balance["Shona"] = 500000000
+	balance["Mehar"] = balance["Gagan"] + balance["Shona"]
+	balance["SomeOneElse"] = 4000
+
+	// Accessing values from the map
+
+	for name, value := range balance {
+		fmt.Printf("%s's balance : %d\n", name, value)
+	}
+
+	fmt.Println("Deleting SomeOneElse From Map")
+
+	delete(balance, "SomeOneElse")
+
+	//Checking if SomeOneElse is present
+	_, present := balance["SomeOneElse"]
+	if !present {
+		fmt.Println("SomeOneElse Removed")
+	}
+
+	for name, value := range balance {
+
+		fmt.Printf("%s Value : %d\n", name, value)
+
+	}
+
+	fmt.Println(len(balance))
+
+}
